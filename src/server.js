2a113c40b3e447a51f941dca931f38c4c8685db5
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2019-04-25 22:14:04 -0300

    Deactivated static file serving.

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const dev = NODE_ENV === 'development';
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
-		sirv('static', { dev }),
+		//sirv('static', { dev }),
 		sapper.middleware()
 	)
 	.listen(PORT, err => {
